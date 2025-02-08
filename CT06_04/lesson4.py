cd2261420bb4a5ba084bdbb67998ac8ed4c777de
Computhink-CS133 <cs133@computhink.com.sg>
2025-02-08 07:23:35 +0000

    Feb 8, 2025, 7:23 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -27,4 +27,4 @@ hobby=input("What do you like to do?")
 
 print("Nice hobby. I might "+hobby+" too!")
 
-Dream_place_to_go=input("If you could go ")
+Dream_place_to_go=input("If you could go anywhere on Earth, ")
