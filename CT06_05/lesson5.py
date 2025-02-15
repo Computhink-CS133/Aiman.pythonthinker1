4caf14be80bf36b74fef5c934887c7fb94b8b62e
Computhink-CS133 <cs133@computhink.com.sg>
2025-02-15 06:55:48 +0000

    Feb 15, 2025, 6:55 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,9 +1,10 @@
 # print("Hello from lesson 5")
 
-Sending_to=input("Who are you sending to? ")
+# Sending_to=input("Who are you sending to? ")
 
-age_they_turning=input("What is the age "+Sending_to+" is turning to?")
+# age_they_turning=input("What is the age "+Sending_to+" is turning to?")
 
-Message=input("Write your messages here ")
+# Message=input("Write your messages here ")
 
-print("Happy "+age_they_turning+"th birthday, "+Sending_to+"! "+Message)
+# print("Happy "+age_they_turning+"th birthday, "+Sending_to+"!"+Message)
+
