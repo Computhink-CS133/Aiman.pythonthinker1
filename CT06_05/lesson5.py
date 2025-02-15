03fcea078e939c705134b6c0cfbf9c7590c06338
Computhink-CS133 <cs133@computhink.com.sg>
2025-02-15 06:46:44 +0000

    Feb 15, 2025, 6:46 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -4,4 +4,5 @@ Sending_to=input("Who are you sending to? ")
 
 age_they_turning=input("What is the age "+Sending_to+" is turning to?")
 
-Message=input("W")
+Message=input("Write your messages here ")
+
