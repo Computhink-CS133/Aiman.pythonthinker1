39324a133cc529e5d41ac4d857fcb29507429919
Computhink-CS133 <cs133@computhink.com.sg>
2025-02-22 07:09:39 +0000

    Feb 22, 2025, 7:09 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,14 +1,39 @@
 # print("Hello from lesson 6")
 
-students=int(input("How many students are there?"))
+# students=int(input("How many students are there?"))
 
-score=0
+# score=0
 
-for i in range(students):
-    score=score+int(input("The score of this student? "))
+# for i in range(students):
+#     score=score+int(input("The score of this student? "))
 
-Average=score/students
+# Average=score/students
 
-print("The average is")
-print(int(Average))
+# print("The average is")
+# print(int(Average))
 
+# Fix the errors in the following:
+
+# **Task 3a**:
+# age = "25"
+# print(age + 1)
+
+# **Task 3b**:
+# number = 10
+# print(number - "5")
+
+# **Task 3c**:
+# print("Repeat" * "3")
+
+# **Task 3d**:
+# year = 2023
+# print("The year is " + year)
+
+# **Task 3e**:
+# x = "10"
+# y = x / 2
+
+# **Task 3f**:
+# end = "5"
+# for i in range(end):
+#     print(i)
