cd19e9f7f29c9851aa0a596b83b6702bb4f6c167
Computhink-CS133 <cs133@computhink.com.sg>
2025-02-22 07:20:42 +0000

    Feb 22, 2025, 7:20 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -13,20 +13,20 @@
 # print(int(Average))
 
 # **Task 1a**:
-for i in range(3):
-    print("Hello, World!")
+# for i in range(3):
+#     print("Hello, World!")
 
-#**Task 1b**:
-for i in range(5):
-    print(i)
-#**Task 1c**:
-print("Hello, World!")
+# #**Task 1b**:
+# for i in range(5):
+#     print(i)
+# #**Task 1c**:
+# print("Hello, World!")
 
-#**Task 1d**:
-ii = 5
+# #**Task 1d**:
+# ii = 5
 
-#**Task 1e**:
-print("Hello, World!")
+# #**Task 1e**:
+# print("Hello, World!")
 
  # **Task 3a**:
 # age = 25
