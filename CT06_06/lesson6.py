19361842a9668185a7b5803f6631f900a8cb1b36
Computhink-CS133 <cs133@computhink.com.sg>
2025-02-22 07:19:13 +0000

    Feb 22, 2025, 7:19 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -12,33 +12,48 @@
 # print("The average is")
 # print(int(Average))
 
-# Fix the errors in the following:
+# **Task 1a**:
+for i in range(3):
+    print("Hello, World!")
 
-# **Task 3a**:
-age = 25
-print(age + 1)
-
-
-
-# **Task 3b**:
-number = 10
-print(number - 5)
-
-
-
-# **Task 3c**:
-print("Repeat" * "3")
-
-
-# **Task 3d**:
-year = 2023
-print("The year is " + year)
-
-# **Task 3e**:
-x = "10"
-y = x / 2
-
-# **Task 3f**:
-end = "5"
-for i in range(end):
+#**Task 1b**:
+for i in range(5):
     print(i)
+#**Task 1c**:
+print("Hello, World!")
+
+#**Task 1d**:
+ii = 5
+
+#**Task 1e**:
+print("Hello, World!")
+
+ # **Task 3a**:
+# age = 25
+# print(age + 1)
+
+
+
+# # **Task 3b**:
+# number = 10
+# print(number - 5)
+
+
+
+# # **Task 3c**:
+# for i in r
+# print("Repeat" * "3")
+
+
+# # **Task 3d**:
+# year = 2023
+# print("The year is " + year)
+
+# # **Task 3e**:
+# x = "10"
+# y = x / 2
+
+# # **Task 3f**:
+# end = "5"
+# for i in range(end):
+#     print(i)
