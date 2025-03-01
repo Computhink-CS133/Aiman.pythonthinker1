604574a2ad9fc2450413c4082d4a5821ed3cad2a
Computhink-CS133 <cs133@computhink.com.sg>
2025-03-01 07:16:48 +0000

    Mar 1, 2025, 7:16 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -1,46 +1,47 @@
-# # # print("Hello from lesson 7")
+# # # # print("Hello from lesson 7")
 
 
-# # # Lesson 7 - For Loop (Part 2)
+# # # # Lesson 7 - For Loop (Part 2)
 
-# # ## Recap 1: Debugging Average Score Program
+# # # ## Recap 1: Debugging Average Score Program
 
-# # ### There is a total of 3 bugs in the following program.
-# # ### Identify and fix the bugs:
+# # # ### There is a total of 3 bugs in the following program.
+# # # ### Identify and fix the bugs:
 
-# # score_one = 80
-# # score_two = 90
-# # score_three = 75
+# # # score_one = 80
+# # # score_two = 90
+# # # score_three = 75
 
-# # total = score_one + score_two + score_three
+# # # total = score_one + score_two + score_three
 
-# # average_score = total / 3
+# # # average_score = total / 3
 
-# # student_name = "Alex"
+# # # student_name = "Alex"
 
-# # print("Average score for " + student_name + " is: " + str(round(average_score,2)))
+# # # print("Average score for " + student_name + " is: " + str(round(average_score,2)))
 
-# for i in range(1,11):
-#     print(i) #Prints out i variable
+# # for i in range(1,11):
+# #     print(i) #Prints out i variable
 
-# for i in range(2,21,2):
-#     print(i) #Prints out i variable
+# # for i in range(2,21,2):
+# #     print(i) #Prints out i variable
 
-# for i in range(10,0,-1):
-#     print(i) #Prints out i variable
+# # for i in range(10,0,-1):
+# #     print(i) #Prints out i variable
 
-# word=input("Whatttttt isssss urrrrrrr nameeeeeee????????  ")
+# # word=input("Whatttttt isssss urrrrrrr nameeeeeee????????  ")
 
-# rep=input("Oooookay! How many names of that name do you want? Write a value. ")
+# # rep=input("Oooookay! How many names of that name do you want? Write a value. ")
 
-# for i in range(int(rep)):
-#     print("Hello thereee "+word)
+# # for i in range(int(rep)):
+# #     print("Hello thereee "+word)
 
 
 
-sum=0
+# sum=0
 
-for i in range(1,6):
-    sum=sum+int(input("What is number "+str(i)+"?"))
+# for i in range(1,6):
+#     sum=sum+int(input("What is number "+str(i)+"?"))
 
-print(sum)
+# print(sum)
+
