54c42440e0231b8f0d11c3f8346fc97ef53561d9
Computhink-CS133 <cs133@computhink.com.sg>
2025-03-01 07:22:21 +0000

    Mar 1, 2025, 7:22 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -64,4 +64,4 @@ num=input("What is da number for da time table???????")
 number=input("How long is da time table?")
 
 for i in range(1,number):
-    print(num+"X"+ str(i)+str(int(num)*int(number)))
+    print(num+"X"+ str(i)+"="+str(int(num)*int(number)))
