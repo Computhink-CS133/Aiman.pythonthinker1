376eceb1513326dc5c687bdf70edc4856c9bad65
Computhink-CS133 <cs133@computhink.com.sg>
2025-03-01 06:55:41 +0000

    Mar 1, 2025, 6:55 AM

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -29,7 +29,7 @@
 # for i in range(10,0,-1):
 #     print(i) #Prints out i variable
 
-word=input("ur name?  ")
+word=input("Whatttttt isssss ur name?  ")
 
 rep=input("Oooookay! How many names of that name do you want? Write a value. ")
 
