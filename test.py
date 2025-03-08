05ada81f5ef717d83709cd5549a8d0a9dc9744bd
Computhink-CS133 <cs133@computhink.com.sg>
2025-03-08 06:28:22 +0000

    Mar 8, 2025, 6:28 AM

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ Start=int(input("What number do you want to start with "))
 Stop=int(input("What number do you want to stop with "))
 step=int(input("What is the increament "))
 
-for i in range(Start,Stop+1,step)
+for i in range(Start,Stop+1,step):
+    print(i)
 
