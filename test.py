6339c3e89f2837b1e143ff70c6ce3125792b4452
Computhink-CS133 <cs133@computhink.com.sg>
2025-03-08 06:33:08 +0000

    Mar 8, 2025, 6:33 AM

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 Start=int(input("What number do you want to start with "))#ask for start number and converts start number to integer
-Stop=int(input("What number do you want to stop with "))#ask for stop number and converts start number to integer
-step=int(input("What is the increament "))#ask for increament
+Stop=int(input("What number do you want to stop with "))#ask for stop number and converts stop number to integer
+step=int(input("What is the increament "))#ask for increamentand converts start number to integer
 
 for i in range(Start,Stop,step):
     print(i)
