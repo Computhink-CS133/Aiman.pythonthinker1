94b552ceb77747ba05cb9544bd880af0fa4f68af
Computhink-CS133 <cs133@computhink.com.sg>
2025-03-15 07:28:27 +0000

    Mar 15, 2025, 7:28 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -30,10 +30,10 @@
 
 import random
 
-num1=random.randint(1.50)
+num1=random.randint(1,50)
 num2=random.randint(1,50)
 
-ans=int(input("What is"+ str(num1)+"+"+str(num2)+"? "))
+ans=int(input("What is "+ str(num1)+"+"+str(num2)+"? "))
 
 if (ans==num1+num2):
     print("correct")
