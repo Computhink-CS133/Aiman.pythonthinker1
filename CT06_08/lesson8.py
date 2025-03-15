bde574d895f99719615b89b0a9f932866e0aac7b
Computhink-CS133 <cs133@computhink.com.sg>
2025-03-15 07:29:35 +0000

    Mar 15, 2025, 7:29 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -33,7 +33,7 @@ import random
 num1=random.randint(1,50)
 num2=random.randint(1,50)
 
-rounds=input("How many questions do you want?")
+rounds=input("How many questions do you want? ")
 
 ans=int(input("What is "+ str(num1)+"+"+str(num2)+"? "))
 
