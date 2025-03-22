ace23d84dc6b61e1c8c58fbab68bb1f41b40bb2f
Computhink-CS133 <cs133@computhink.com.sg>
2025-03-22 06:29:04 +0000

    Mar 22, 2025, 6:29 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,12 +1,2 @@
 # print("Hello from lesson 9")
 
-import random as rand
-
-num=str(rand.randint(1,10))
-guess=input("Pick a random number between 1 to 10."
-" ")
-
-print("Your guess: "+ guess)
-print("The number: "+ num)
-
-print(guess==num)
