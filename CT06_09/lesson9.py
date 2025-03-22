b19579eb4f1effac7073a98d2508befe980b6715
Computhink-CS133 <cs133@computhink.com.sg>
2025-03-22 06:21:55 +0000

    Mar 22, 2025, 6:21 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -5,4 +5,5 @@ import random as rand
 num1=str(rand.randint(1,10))
 guess=input("Pick a random number between 1 to 10")
 
-print("Your guess: ")
+print("Your guess: "+ guess)
+print("Your guess: "+ guess)
