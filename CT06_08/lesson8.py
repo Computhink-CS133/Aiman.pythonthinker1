550439daa2f2ed32eb4edfe5c6d361fb29ec7a1b
Computhink-CS133 <cs133@computhink.com.sg>
2025-03-22 06:36:09 +0000

    Mar 22, 2025, 6:36 AM

--- CT06_08/lesson8.py
+++ CT06_08/lesson8.py
@@ -50,7 +50,7 @@ rounds=int(input("How many questions do you want? "))
 
 for i in range(rounds):
     num1=random.randint(1.10)
-    num2=r.randint(1.10)
+    num2=random.randint(1.10)
     Ans=int(input("What is "+str(num1)+" x "+str(num2)+"? "))
     print("Your answer is"+ str(Ans==num1*num2))
     print("The answer is"+str(num1*num2))
