be9d415135d72cb9414806345387afa1ef99acd4
Computhink-CS133 <cs133@computhink.com.sg>
2025-03-22 07:05:01 +0000

    Mar 22, 2025, 7:05 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,15 +1,15 @@
-# print("Hello from lesson 9")
+# # print("Hello from lesson 9")
 
-import random as r
+# import random as r
 
-num1=r.randint(1,6)
-num2=r.randint(1,6)
-num3=r.randint(1,6)
-alleven=num1 % 2==0 and num2 % 2==0 and num3 % 2==0
-allodd=num1 % 2==1 and num2 % 2==1 and num3 % 2==1
+# num1=r.randint(1,6)
+# num2=r.randint(1,6)
+# num3=r.randint(1,6)
+# alleven=num1 % 2==0 and num2 % 2==0 and num3 % 2==0
+# allodd=num1 % 2==1 and num2 % 2==1 and num3 % 2==1
 
-print("Number 1: "+str(num1) )
-print("Number 2: "+str(num2) )
-print("Number 3: "+str(num3) )
-print("All numbers are even: "+str(alleven))
-print("All numbers are odd: "+str(allodd))
+# print("Number 1: "+str(num1) )
+# print("Number 2: "+str(num2) )
+# print("Number 3: "+str(num3) )
+# print("All numbers are even: "+str(alleven))
+# print("All numbers are odd: "+str(allodd))
