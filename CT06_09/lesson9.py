95723964fdcecc87af495f75be575b797370c762
Computhink-CS133 <cs133@computhink.com.sg>
2025-03-22 06:52:05 +0000

    Mar 22, 2025, 6:52 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -2,15 +2,6 @@
 
 import random as r
 
-# rounds=int(input("How many questions do you want? "))
-
-# for i in range(rounds):
-#     num1=r.randint(1,10)
-#     num2=r.randint(1,10)
-#     Ans=int(input("What is "+str(num1)+" x "+str(num2)+"? "))
-#     print("Your answer is "+ str(Ans==num1*num2))
-#     print("The answer is "+str(num1*num2))
-
 num1=r.randint(1,6)
 num2=r.randint(1,6)
 num3=r.randint(1,6)
