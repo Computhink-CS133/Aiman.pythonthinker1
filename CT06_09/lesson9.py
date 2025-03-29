a3db9cac9a96980fb6da8c1e5a271c05b2f1f33e
Computhink-CS133 <cs133@computhink.com.sg>
2025-03-29 06:18:18 +0000

    Mar 29, 2025, 6:18 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,47 +1,47 @@
-# # print("Hello from lesson 9")
+# # # print("Hello from lesson 9")
 
-# import random as r
+# # import random as r
 
-# num1=r.randint(1,6)
-# num2=r.randint(1,6)
-# num3=r.randint(1,6)
-# alleven=num1 % 2==0 and num2 % 2==0 and num3 % 2==0
-# allodd=num1 % 2==1 and num2 % 2==1 and num3 % 2==1
+# # num1=r.randint(1,6)
+# # num2=r.randint(1,6)
+# # num3=r.randint(1,6)
+# # alleven=num1 % 2==0 and num2 % 2==0 and num3 % 2==0
+# # allodd=num1 % 2==1 and num2 % 2==1 and num3 % 2==1
 
-# print("Number 1: "+str(num1) )
-# print("Number 2: "+str(num2) )
-# print("Number 3: "+str(num3) )
-# print("All numbers are even: "+str(alleven))
-# print("All numbers are odd: "+str(allodd))
+# # print("Number 1: "+str(num1) )
+# # print("Number 2: "+str(num2) )
+# # print("Number 3: "+str(num3) )
+# # print("All numbers are even: "+str(alleven))
+# # print("All numbers are odd: "+str(allodd))
 
-# DAYS=int(input("How many days have you been borrowing the book? "))
+# # DAYS=int(input("How many days have you been borrowing the book? "))
 
-# if DAYS>25:
-#     print("Remember to return it.")
-
-
-
-# import random as r
-
-# num=r.randint(1,10)
-# ans=int(input("Guess the number "))
-# if num==ans:
-    # print("Hooray")
+# # if DAYS>25:
+# #     print("Remember to return it.")
 
 
+
+# # import random as r
+
+# # num=r.randint(1,10)
+# # ans=int(input("Guess the number "))
+# # if num==ans:
+#     # print("Hooray")
 
 
 
 
-numapples=int(input("How many apples do you want? "))
-numoranges=int(input("How many oranges do you want? "))
 
-appleprice=0.60*numapples
-if numapples>5:
-    appleprice=(appleprice-(appleprice/10))
 
-orangeprice=0.90*numoranges
-if numoranges>5:
-    orangeprice=(orangeprice-(orangeprice/10))
+# numapples=int(input("How many apples do you want? "))
+# numoranges=int(input("How many oranges do you want? "))
 
-print(appleprice+orangeprice)
+# appleprice=0.60*numapples
+# if numapples>5:
+#     appleprice=(appleprice-(appleprice/10))
+
+# orangeprice=0.90*numoranges
+# if numoranges>5:
+#     orangeprice=(orangeprice-(orangeprice/10))
+
+# print(appleprice+orangeprice)
