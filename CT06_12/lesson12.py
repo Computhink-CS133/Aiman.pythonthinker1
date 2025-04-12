252bcd939913d72c88eedc0af633fd719da05c3b
Computhink-CS133 <cs133@computhink.com.sg>
2025-04-12 06:44:21 +0000

    Apr 12, 2025, 6:44 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -5,12 +5,18 @@
 #     visitors+=1
 #     print(visitors)
 
-max_pax=30
-pax=0
-while True:
-    add_pax=input("Add a visitor?")
-    if add_pax=="yes":
-        pax+=1
-    if pax>max_pax:
-        break
+# max_pax=30
+# pax=0
+# while True:
+#     add_pax=input("Add a visitor?")
+#     if add_pax=="yes":
+#         pax+=1
+#     if pax>max_pax:
+#         break
+
+
+
+
+
+
 
