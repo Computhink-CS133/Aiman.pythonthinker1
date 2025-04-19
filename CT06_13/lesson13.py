4525c4e30b8574e49e1bbf99138f2c6feb5084b8
Computhink-CS133 <cs133@computhink.com.sg>
2025-04-19 06:47:27 +0000

    Apr 19, 2025, 6:47 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,25 +1,33 @@
-money=1000
-amount=0
-while True: 
-    print("Choose a choice number")
-    print("1.Withdraw")
-    print("2.Deposit")
-    print("3.Check money")
-    print("4.Get out")
-    choice=int(input("Choose one "))
-    if choice==1:
-        amount==int(input("How much money you want to withdraw "))
-        if amount<=money:
-            money-=amount
-            print("You have "+ str(money)+" moneys")
-        else:
-            print("An error occured!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    if choice==2:
-        amount==int(input("How much to deposit "))
-        money+=amount
-        print("You have "+ str(money)+" moneys")
-    if choice==3:
-        print("You have "+ str(money)+" moneys")
-    if choice==4:
-        break
-print("You have "+ str(money)+" moneys")
+# money=1000
+# amount=0
+# while True: 
+#     print("Choose a choice number")
+#     print("1.Withdraw")
+#     print("2.Deposit")
+#     print("3.Check money")
+#     print("4.Get out")
+#     choice=int(input("Choose one "))
+#     if choice==1:
+#         amount==int(input("How much money you want to withdraw "))
+#         if amount<=money:
+#             money-=amount
+#             print("You have "+ str(money)+" moneys")
+#         else:
+#             print("An error occured!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+#     if choice==2:
+#         amount==int(input("How much to deposit "))
+#         money+=amount
+#         print("You have "+ str(money)+" moneys")
+#     if choice==3:
+#         print("You have "+ str(money)+" moneys")
+#     if choice==4:
+#         break
+# print("You have "+ str(money)+" moneys")
+
+
+
+
+
+
+
+
