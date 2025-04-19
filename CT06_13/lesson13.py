22bf414c94e93b321e5f7f85b083a654696c1520
Computhink-CS133 <cs133@computhink.com.sg>
2025-04-19 06:39:59 +0000

    Apr 19, 2025, 6:39 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -13,7 +13,7 @@ while True:
             money-=amount
             print("You have "+ str(money)+" moneys")
         else:
-            print("An error ")
+            print("An error occured!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     if choice==2:
         amount==int(input("How much to deposit "))
         money+=amount
