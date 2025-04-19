160ccea0cd9d85f1d7f6226e2e6898a947412848
Computhink-CS133 <cs133@computhink.com.sg>
2025-04-19 07:23:18 +0000

    Apr 19, 2025, 7:23 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -46,7 +46,7 @@ list_of_stuff=[
 list_of_stuff.append("ice")
 list_of_stuff.insert(3,"yellow bananas")
 list_of_stuff[8]="herbs"
-del(list_of_stuff)
+del(list_of_stuff[10])
 for i in range(len(list_of_stuff)):
     print(list_of_stuff[i])
 
