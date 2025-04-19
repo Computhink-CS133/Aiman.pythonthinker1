dcdb3f861072e30e21e80a5dceb7d0a155415082
Computhink-CS133 <cs133@computhink.com.sg>
2025-04-19 06:27:43 +0000

    Apr 19, 2025, 6:27 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -3,7 +3,7 @@ while True:
     print("Choose a choice number")
     print("1.Withdraw")
     print("2.Deposit")
-print("3.Check money")
-print("4.Get out")
+    print("3.Check money")
+    print("4.Get out")
 
 i
