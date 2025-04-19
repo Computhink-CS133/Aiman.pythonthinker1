d640e6d1466bb92074f20f9f77f5646cc1c2eb01
Computhink-CS133 <cs133@computhink.com.sg>
2025-04-19 06:37:52 +0000

    Apr 19, 2025, 6:37 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -10,7 +10,7 @@ while True:
     if choice==1:
         amount==int(input("How much money you want to withdraw "))
         if amount<=money:
-            money=amount
+            money=money-amount
             print("You have "+ str(money)+" moneys")
     if choice==2:
         amount==int(input("How much to deposit "))
