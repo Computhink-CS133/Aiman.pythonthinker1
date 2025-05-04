55e1f53953d81d15b2cb513ffaae89c8037a4e9b
Computhink-CS133 <cs133@computhink.com.sg>
2025-05-04 08:53:35 +0000

    May 4, 2025, 8:53 AM

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-planets = ["mercury","venus", "earth", "mars", "jupiter", "saturn", "uranus" ]
-
-print(planets[2])
-
-
-planets.append("neptune")
-
-planets[3]="muskworld"
-
-planets.pop(6)
-
-for i in planets:
-    print(i)
+name = i
 
