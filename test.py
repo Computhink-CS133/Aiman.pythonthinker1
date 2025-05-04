9e0cc0106d31366f4030037946152bf9ae5efc93
Computhink-CS133 <cs133@computhink.com.sg>
2025-05-04 08:27:58 +0000

    May 4, 2025, 8:27 AM

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 password="superpass123"
 
-userinput=input("What is the password")
+userinput=input("What is the password? ")
 
 if userinput==password:
     print("Access Granted!")
 else:
-    print("Access Denied")
+    print("Access Denied!")
 
 
 
