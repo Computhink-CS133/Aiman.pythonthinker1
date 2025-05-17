d905432ce08fde4c42813fed1328bb8ec6136837
Computhink-CS133 <cs133@computhink.com.sg>
2025-05-17 06:43:50 +0000

    May 17, 2025, 6:43 AM

--- CT06_15/lesson15.py
+++ CT06_15/lesson15.py
@@ -1,10 +1,10 @@
 # print("Hello from lesson 14")
 counter=0
 
-def increase():
+def increase(num):
     global counter 
     counter +=1
 for i in range(3):
-    increase()
+    increase(1)
     
 print(counter)
