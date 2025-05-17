f983a904e705b602c606f07cf14ef8839ba8a41e
Computhink-CS133 <cs133@computhink.com.sg>
2025-05-17 06:11:51 +0000

    May 17, 2025, 6:11 AM

--- CT06_14/lesson14.py
+++ CT06_14/lesson14.py
@@ -4,12 +4,12 @@ import turtle
 window = turtle.Screen()
 
 window.setup(width=600,height=400)
-t=turtle.Turtle()
-t.shape("turtle")
-t.fillcolor("orange")
+# t=turtle.Turtle()
+# t.shape("turtle")
+# t.fillcolor("orange")
 
-for i in range(1):
-    t.circle(90)
+# for i in range(1):
+#     t.circle(90)
     
 
 window.mainloop()
