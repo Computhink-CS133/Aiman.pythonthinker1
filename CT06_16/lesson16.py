8e564ec8c5b93acfd946e00f03693718b906db24
Computhink-CS133 <cs133@computhink.com.sg>
2025-05-24 06:31:46 +0000

    May 24, 2025, 6:31 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -2,13 +2,6 @@ import turtle
 
 window=turtle.Screen()
 
-t=turtle.Turtle()
 
-def DrawShapes(length,num_sides):
-    for i in range(num_sides):
-        t.forward(length)
-        t.left(360/num_sides)
-
-DrawShapes(100,3)
 
 window.mainloop()
