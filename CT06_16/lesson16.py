a31780aaee386a6b3ab2098db4f23359dc56c672
Computhink-CS133 <cs133@computhink.com.sg>
2025-05-24 06:34:23 +0000

    May 24, 2025, 6:34 AM

--- CT06_16/lesson16.py
+++ CT06_16/lesson16.py
@@ -2,7 +2,8 @@ import turtle
 def setupwindow(width,height):
     window=turtle.Screen()
     window.setup(width,height)
+    # return the window here
 
-setupwindow()
+setupwindow(300,500)
 
 window.mainloop()
